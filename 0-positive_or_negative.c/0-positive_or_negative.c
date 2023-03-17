23d21e964cfbc75064d3511adf3caac63d7c2f0a
islammohmed <islammohamed3235@gmail.com>
2023-03-17 16:27:21 +0200

    done

---
+++ 0-positive_or_negative.c/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+*main-Entry point
+*
+*Return:Alwys 0 (Success)
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("is positive");
+else if (n == 0)
+printf("is zero");
+else
+printf("is negative");
+return (0);
+}
