634eff649e610bb46c92b3df00614f6a9eb4b513
islammohmed <islammohamed3235@gmail.com>
2023-03-18 01:44:18 +0200

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main-Entry point
+*
+*Return:Alwys 0 (Success)
+*/
+int main(void)
+{
+for (int i = 97 ; i <= 122 ; i++)
+{
+putchar(i);
+}
+}
