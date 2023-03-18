a7241078e6699d2ba2c6ae9f9130d45b2a18b1bf
islammohmed <islammohamed3235@gmail.com>
2023-03-18 02:12:45 +0200

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main-Entry point
+*
+*Return:Alwys 0 (Success)
+*/
+int main(void)
+{
+char i;
+for (i = 'a' ; i <= 'z'; i++)
+{
+if (i != 'q' && i != 'e')
+putchar(i);
+}
+}
