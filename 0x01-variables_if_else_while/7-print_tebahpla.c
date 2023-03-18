b1f92469651ffb0e9584f3d8f51a223114666c91
islammohmed <islammohamed3235@gmail.com>
2023-03-18 03:00:19 +0200

    done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main-Entry point
+*
+*Return:Alwys 0 (Success)
+*/
+int main(void)
+{
+char i;
+for (i = 'z' ; i >= 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
