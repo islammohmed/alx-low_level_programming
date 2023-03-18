44db5ef9d9ff79f8995c29f28ac26bc849916e14
islammohmed <islammohamed3235@gmail.com>
2023-03-18 03:28:45 +0200

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main-Entry point
+*
+*Return:Alwys 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = '0' ; i <= '9' ; i++)
+{
+putchar(i);
+if (i != '9')
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
