cf6dd606970a35ffd2b2990e9122907e9e59d5f6
islammohmed <islammohamed3235@gmail.com>
2023-03-23 22:52:30 +0200

    done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal -  a function that print number from 0 to 9 expt 2 and 4
+ *@n:input
+ * Return: the numbers
+ */
+void print_diagonal(int n)
+{
+if (n > 0)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+int j;
+for (j = 0; j < i; j++)
+{
+printf(" ");
+}
+printf("\\");
+printf("\n");
+}
+}
+printf("\n");
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+void print_diagonal(int n);
 void more_numbers(void);
 void print_numbers(void);
 void print_line(int n);
