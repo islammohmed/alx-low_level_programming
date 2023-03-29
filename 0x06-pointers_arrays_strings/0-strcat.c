aa44a9dbdcb6a6b86776c411d25d29a49e9fbbf4
islammohmed <islammohamed3235@gmail.com>
2023-03-29 23:14:28 +0200

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,5 +7,11 @@
 */
 char *_strcat(char *dest, char *src)
 {
-return (strcat(dest, src));
+int c, c2;
+c = 0;
+while (dest[c])
+c++;
+for (c2 = 0; src[c2] ; c2++)
+dest[c++] = src[c2];
+return (dest);
 }
