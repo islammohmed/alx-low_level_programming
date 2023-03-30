9ec90d1eadb85bbb43eb2b95eff5e097a65964e0
islammohmed <islammohamed3235@gmail.com>
2023-03-31 01:33:45 +0200

    done

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,26 +1,28 @@
+
 #include "main.h"
+
 /**
- * rot13 - cencode a string using rot13
- * @c: string
- * Return: encode of s
+ * rot13 - encodes a string into rot13
+ * @s: string to encode
+ *
+ * Return: address of s
  */
-char *rot13(char *c)
+char *rot13(char *s)
 {
-int *cp = c;
-int i;
-char key[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char value[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-while (*c)
-{
-for (i = 0; i <= 52; i++)
-{
-if (*c == key[i])
-{
-*c = value[i];
-break;
-}
-}
-c++;
-}
-return (cp);
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
