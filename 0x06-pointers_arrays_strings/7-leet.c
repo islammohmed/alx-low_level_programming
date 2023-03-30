cc237bb45bca5dd89bf4b34d5c9d5e665fa294e8
islammohmed <islammohamed3235@gmail.com>
2023-03-30 23:59:41 +0200

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,24 @@
 #include "main.h"
+
 /**
- * leet - capitalizes all words in a string
- * @c: string
- * Return: encode of s
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: address of s
  */
-char *leet(char *c)
+char *leet(char *s)
 {
-int *cp = c;
-unsigned int i;
-char key[] = {'A', 'E', 'O', 'T', 'L'};
-int value[] = {4, 3, 0, 7, 1};
-while (*c)
-{
-for (i = 0; i < sizeof(key) / sizeof(char); i++)
-{
-if (*c == key[i] || *c == key[i] + 32)
-{
-*c = 48 + value[i];
-}
-}
-c++;
-}
-return (cp);
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == s[i])
+				s[i] = b[j];
+		}
+	}
+	return (s);
 }
